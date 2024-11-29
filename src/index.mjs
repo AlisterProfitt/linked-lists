60a52649a24b17c7a2cff17f752285f7a0c1ad8c
AlisterProfitt <alisterprofitt@gmail.com>
2024-11-29 09:32:50 -0400

    create append method

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,8 @@
 import { LinkedList } from "./linkedListConstructor.mjs";
 
 const list = new LinkedList();
+list.append('dog')
+list.append('spider')
+list.append('spy')
+list.append('cat')
 console.log(list);
--- src/linkedListConstructor.mjs
+++ src/linkedListConstructor.mjs
@@ -5,6 +5,24 @@ class LinkedList {
         this.head = new Node(null);
         this.length = 0;
     }
+
+    append(value) {
+        if (this.head.value === null) {
+            this.head.value = value;
+            this.length++;
+            return
+        }
+        const newNode = new Node(value);
+        let currentNode = this.head;
+        while (currentNode.value !== null) {
+            if (currentNode.nextNode === null) {
+                currentNode.nextNode = newNode;
+                break;
+            }
+            currentNode = currentNode.nextNode;
+        } 
+        this.length++;
+    }
 }
 
 export { LinkedList }
